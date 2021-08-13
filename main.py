ebaf3909178cb30c23302f426e63a4fcd629df3f
kirill belkin <kirillbelkin@bk.ru>
2021-08-13 14:31:55 +0300

    #3 Добавлена возможность выбора действия

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Простой калькулятор
 choose = 1
 while choose != 0:
+    choose = "1 - сложение, 2 - Вычитание, 0 - Выход"
+    choose = int(input(choose))
     if choose == 1:  # Сложение
         pass
     elif choose == 2:  # Вычитание
